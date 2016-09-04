041fbc679112b44e62134f323a1b53c81aa30444
Radomir Dopieralski <openstack@sheep.art.pl>
2016-09-04 14:15:50 +0200

    Add license, readme and setup.py

---
+++ README.rst
@@ -0,0 +1,6 @@
+Adafruit TSL2561 Library
+************************
+
+A MicroPython library for interfacing with the TSL2561 light sensor.
+
+For full documentation see http://micropython-tsl2561.rtfd.io/.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+
+setup(
+    name='Adafruit-tsl2561',
+    py_modules=['tsl2561'],
+    version="1.0",
+    description="Driver for MicroPython for the tsl2561 light sensor.",
+    long_description="""\
+This library lets you communicate with a TSL2561 light sensor.
+""",
+    author='Radomir Dopieralski',
+    author_email='micropython@sheep.art.pl',
+    classifiers = [
+        'Development Status :: 6 - Mature',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
